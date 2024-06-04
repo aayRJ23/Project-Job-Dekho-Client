e3444c42119d1e4e89d9d118793f83fd9e77c437
Aayush Raj <rajaayush248@gmail.com>
2024-06-04 19:01:39 +0530

    Footer UI updated

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,27 +1,37 @@
 import React, { useContext } from "react";
 import { Context } from "../../main";
 import { Link } from "react-router-dom";
-import { FaFacebookF, FaYoutube, FaLinkedin } from "react-icons/fa";
+import { FaFacebookF, FaYoutube, FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
+import "./Footer.css"; // Import the CSS file
 
 const Footer = () => {
   const { isAuthorized } = useContext(Context);
   return (
-    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
-      <div>&copy; All Rights Reserved By - Team Job Dekho ❤️</div>
-      <div>
-        <Link to={"https://www.facebook.com/"} target="_blank">
-          <FaFacebookF />
-        </Link>
-        <Link to={"https://www.youtube.com"} target="_blank">
-          <FaYoutube />
-        </Link>
-        <Link to={"https://www.youtube.com"} target="_blank">
-          <FaLinkedin />
-        </Link>
-        <Link to={"https://www.instagram.com"} target="_blank">
-          <RiInstagramFill />
-        </Link>
+    <footer className={`footer ${isAuthorized ? "footerShow" : "footerHide"}`}>
+      <div className="footerContent">
+        <div>&copy; All Rights Reserved By - Team Job Dekho ❤️</div>
+        <div className="connectText">Connect with us:</div>
+        <div className="socialLinks">
+          <Link to={"https://www.facebook.com/"} target="_blank" className="socialIcon facebook">
+            <FaFacebookF />
+          </Link>
+          <Link to={"https://www.youtube.com"} target="_blank" className="socialIcon youtube">
+            <FaYoutube />
+          </Link>
+          <Link to={"https://www.linkedin.com"} target="_blank" className="socialIcon linkedin">
+            <FaLinkedin />
+          </Link>
+          <Link to={"https://www.instagram.com"} target="_blank" className="socialIcon instagram">
+            <RiInstagramFill />
+          </Link>
+          <Link to={"https://www.github.com"} target="_blank" className="socialIcon github">
+            <FaGithub />
+          </Link>
+          <Link to={"https://www.twitter.com"} target="_blank" className="socialIcon twitter">
+            <FaTwitter />
+          </Link>
+        </div>
       </div>
     </footer>
   );
